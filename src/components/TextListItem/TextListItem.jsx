d7bf5078b96a5addb3d2309b460fd9dfbf3acfe1
Andreas Schickedanz <as@bornageek.com>
2019-07-28 01:22:38 +0200

    Converted TextListItem react component class to component function

--- src/components/TextListItem/TextListItem.jsx
+++ src/components/TextListItem/TextListItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import colors from '../../theme/colors.scss';
 
@@ -6,36 +6,31 @@ import './TextListItem.scss';
 
 const prefixCls = 'kai-tl';
 
-class TextListItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFocused: false,
-    };
-    this.handleFocusChange = this.handleFocusChange.bind(this);
-  }
-
-  handleFocusChange(isFocused) {
-    this.setState({ isFocused });
-    if (isFocused) {
-      this.props.onFocusChange(this.props.index);
-    }
-  }
-
-  render() {
+const TextListItem = React.memo(
+  props => {
     const {
       primary,
       secondary,
       tertiary,
       focusColor,
       forwardedRef,
-    } = this.props;
-    const { isFocused } = this.state;
+      index,
+      onFocusChange
+    } = props;
+
+    const [isFocused, setFocused] = useState(false);
 
     const itemCls = prefixCls;
     const primaryCls = `${prefixCls}-primary`;
     const secondaryCls = `${prefixCls}-secondary ${secondary ? '' : 'hidden'}`;
     const tertiaryCls = `${prefixCls}-tertiary ${tertiary ? '' : 'hidden'}`;
+
+    const handleFocusChange = newFocused => {
+      setFocused(newFocused);
+      if (newFocused) {
+        onFocusChange(index);
+      }
+    }
 
     return (
       <div
@@ -43,8 +38,8 @@ class TextListItem extends React.PureComponent {
         className={itemCls}
         style={{ backgroundColor: isFocused ? focusColor : colors.white }}
         ref={forwardedRef}
-        onFocus={() => this.handleFocusChange(true)}
-        onBlur={() => this.handleFocusChange(false)}
+        onFocus={() => handleFocusChange(true)}
+        onBlur={() => handleFocusChange(false)}
       >
         <span className={primaryCls}>{primary}</span>
         <label className={secondaryCls}>{secondary}</label>
@@ -52,25 +47,25 @@ class TextListItem extends React.PureComponent {
       </div>
     );
   }
-}
-
-TextListItem.defaultProps = {
-  secondary: null,
-  tertiary: null,
-  focusColor: colors.defaultFocusColor,
-};
+);
 
 TextListItem.propTypes = {
   primary: PropTypes.string.isRequired,
   secondary: PropTypes.string,
   tertiary: PropTypes.string,
   focusColor: PropTypes.string,
-  forwardedRed: PropTypes.oneOfType([
+  forwardedRef: PropTypes.oneOfType([
     PropTypes.func,
     PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
   ]),
   index: PropTypes.number,
   onFocusChange: PropTypes.func,
+};
+
+TextListItem.defaultProps = {
+  secondary: null,
+  tertiary: null,
+  focusColor: colors.defaultFocusColor,
 };
 
 export default React.forwardRef((props, ref) => (
