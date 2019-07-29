a1579582edc9d948e946370670009e7068484de5
Andreas Schickedanz <as@bornageek.com>
2019-07-29 23:50:50 +0200

    Converted TabView react component class to component function

--- src/views/TabView/TabView.jsx
+++ src/views/TabView/TabView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import Tabs from '../../components/Tabs/Tabs';
@@ -9,55 +9,54 @@ import './TabView.scss';
 
 const prefixCls = 'kai-tab-view';
 
-class TabView extends React.PureComponent {
-  constructor() {
-    super();
-    this.renderChildren = this.renderChildren.bind(this);
-    this.renderTabs = this.renderTabs.bind(this);
-    this.handleChangeIndex = this.handleChangeIndex.bind(this);
-    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
-    this.state = { activeTab: 0, isTransitionDone: true };
-  }
+const TabView = React.memo(
+  props => {
+    const {
+      tabLabels,
+      onChangeIndex,
+      focusColor,
+      children
+    } = props;
 
-  handleChangeIndex(tabIndex) {
-    this.setState({ activeTab: tabIndex, isTransitionDone: false });
-    this.props.onChangeIndex(tabIndex);
-  }
+    const [activeTab, setActiveTab] = useState(0);
+    const [isTransitionDone, setTransitionDone] = useState(true);
 
-  handleTransitionEnd() {
-    this.setState({ isTransitionDone: true });
-  }
-
-  renderTabs() {
-    return this.props.tabLabels.map((label, i) => {
-      return (
-        <Tab
-          key={`key-${i}`}
-          label={label}
-          focusColor={this.props.focusColor}
-        />
-      );
-    });
-  }
-
-  renderChildren() {
-    const { activeTab, isTransitionDone } = this.state;
-    return React.Children.map(this.props.children, (child, i) => {
-      return React.cloneElement(child, {
-        isActive: activeTab === i && isTransitionDone,
-      });
-    });
-  }
-
-  render() {
-    const { activeTab } = this.state;
     const tabViewTabs = `${prefixCls}-tabs`;
     const tabViewContent = `${prefixCls}-content`;
+
+    const handleChangeIndex = tabIndex => {
+      setActiveTab(tabIndex);
+      setTransitionDone(false);
+      onChangeIndex(tabIndex);
+    };
+
+    const handleTransitionEnd = () => setTransitionDone(true);
+
+    const renderTabs = () => {
+      return tabLabels.map((label, i) => {
+        return (
+          <Tab
+            key={`key-${i}`}
+            label={label}
+            focusColor={focusColor}
+          />
+        );
+      });
+    };
+
+    const renderChildren = () => {
+      return React.Children.map(children, (child, i) => {
+        return React.cloneElement(child, {
+          isActive: activeTab === i && isTransitionDone,
+        });
+      });
+    };
+
     return (
       <div className={prefixCls}>
         <div className={tabViewTabs}>
-          <Tabs onChangeIndex={this.handleChangeIndex}>
-            {this.renderTabs()}
+          <Tabs onChangeIndex={handleChangeIndex}>
+            {renderTabs()}
           </Tabs>
         </div>
 
@@ -65,27 +64,28 @@ class TabView extends React.PureComponent {
           <SwipeableViews
             axis={'x'}
             index={activeTab}
-            onChangeIndex={this.handleChangeIndex}
-            onTransitionEnd={this.handleTransitionEnd}
+            onChangeIndex={handleChangeIndex}
+            onTransitionEnd={handleTransitionEnd}
             disabled={true}
           >
-            {this.renderChildren()}
+            {renderChildren()}
           </SwipeableViews>
         </div>
       </div>
     );
   }
-}
-
-TabView.defaultProps = {
-  onChangeIndex: () => {},
-  focusColor: colors.defaultFocusColor,
-};
+);
 
 TabView.propTypes = {
   tabLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChangeIndex: PropTypes.func,
   focusColor: PropTypes.string,
+  children: PropTypes.array,
+};
+
+TabView.defaultProps = {
+  onChangeIndex: () => {},
+  focusColor: colors.defaultFocusColor,
 };
 
 export default TabView;
