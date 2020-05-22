284a0edb8fe552a2580b7cc5110d9e556f166865
Patrick J Burke <patburke234@gmail.com>
2020-05-22 10:04:40 +0400

    Scrolling List View, TriColumn List View

---
+++ src/views/ScrollingListView/ScrollingListView.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import './ScrollingListView.scss';
+import classNames from 'classnames';
+import BodyTextListItem from '../../components/BodyTextListItem/BodyTextListItem';
+
+const prefixCls = 'kai-scroll-list-view';
+
+interface LocalProps {
+  children: any[];
+  onChangeIndex?: (index: number) => void;
+  isActive: boolean;
+  className?: string;
+  initialSelectedIndex?: number;
+}
+
+const ScrollingListView = React.memo<LocalProps>(
+  (props) => {
+    const {
+      children,
+      onChangeIndex,
+      isActive,
+      className,
+      initialSelectedIndex
+    } = props;
+
+    const [activeItem, setActiveItem] = useState(initialSelectedIndex === undefined ? 1 : initialSelectedIndex);
+
+    const handleChangeIndex = itemIndex => {
+      if(onChangeIndex)
+        onChangeIndex(itemIndex);
+    };
+
+    const handleKeyDown = useCallback(
+      e => {
+        let index = activeItem;
+        if (!isActive) {
+          return;
+        }
+
+        switch (e.key) {
+          case 'ArrowUp':
+            index = index - 1 >= 0 ? index - 1 : index;
+            setActiveItem(index);
+            handleChangeIndex(index);
+            break;
+          case 'ArrowDown':
+            index = index + 1 < children.length ? index + 1 : index;
+            setActiveItem(index);
+            handleChangeIndex(index);
+            break;
+          default:
+            break;
+        }
+      },
+      [isActive, activeItem, setActiveItem]
+    );
+
+    useEffect(
+      () => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => document.removeEventListener('keydown', handleKeyDown);
+      },
+      [handleKeyDown]
+    );
+
+    const renderChildren = () => {
+      const childrenToDisplay = children.filter((item, i) => i >= activeItem - 1 && i <= activeItem + 1);
+
+      if(childrenToDisplay.length == 2){
+        if(activeItem == 0){
+          childrenToDisplay.unshift(<BodyTextListItem header=""/>);
+        }
+        else if(activeItem == children.length){
+          childrenToDisplay.push(<BodyTextListItem header=""/>);
+        }
+      }
+
+      let index = -1;
+      return React.Children.map(childrenToDisplay, child => {
+        // Don't focus on separators
+        if (!child || child.props.separatorText != null) {
+          return child;
+        }
+        index++;
+        const newRef = React.createRef();
+        return React.cloneElement(child, {
+          index,
+          ref: newRef,
+          className: classNames(
+                        index == 1 ? prefixCls + "-middleItem" : prefixCls + "-nonMiddleItem",
+                        index == 1 && isActive ? prefixCls + "-activeItem" : ''
+                     )
+        });
+      });
+    };
+
+    return <div className={classNames(prefixCls, (className||''))}>{renderChildren()}</div>;
+  }
+);
+
+export default ScrollingListView;
---
+++ src/views/TriColumnListView/TriColumnListView.tsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import './TriColumnListView.scss';
+import ListView from '../ListView/ListView';
+import ScrollingListView from '../ScrollingListView/ScrollingListView';
+
+const prefixCls = 'kai-tricol-view';
+
+interface LocalProps {
+  onChangeIndex?: (index: number) => void,
+  focusColor?: string,
+  col1Children: any[],
+  col2Children: any[],
+  col3Children: any[],
+  onCol1ChangeIndex?: (index: number) => void,
+  onCol2ChangeIndex?: (index: number) => void,
+  onCol3ChangeIndex?: (index: number) => void,
+  selectedCol1Index?: number,
+  selectedCol2Index?: number,
+  selectedCol3Index?: number
+}
+
+const TriColListView = React.memo<LocalProps>(
+  props => {
+    const {
+      onChangeIndex,
+      focusColor,
+      col1Children,
+      col2Children,
+      col3Children,
+      onCol1ChangeIndex,
+      onCol2ChangeIndex,
+      onCol3ChangeIndex,
+      selectedCol1Index,
+      selectedCol2Index,
+      selectedCol3Index
+    } = props;
+
+    const [activeTab, setActiveTab] = useState(0);
+    const [isTransitionDone, setTransitionDone] = useState(true);
+
+    const handleChangeIndex = tabIndex => {
+      // NOTE: Ensure you set state for tab transition first.
+      //       Otherwise you will face strange race condition bugs.
+      setTransitionDone(false);
+      setActiveTab(tabIndex);
+      if(onChangeIndex)
+        onChangeIndex(tabIndex);
+    };
+
+    
+    const handleKeyDown = useCallback(
+      e => {
+        let index = activeTab;
+        switch (e.key) {
+          case 'ArrowLeft':
+            index = index - 1 >= 0 ? index - 1 : 3 - 1;
+            setActiveTab(index);
+            break;
+          case 'ArrowRight':
+            index = index + 1 < 3 ? index + 1 : 0;
+            setActiveTab(index);
+          default:
+            break;
+        }
+      },
+      [activeTab, setActiveTab]
+    );
+
+    useEffect( 
+      () => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => document.removeEventListener('keydown', handleKeyDown);
+      },
+      [handleKeyDown]
+    );
+
+    const renderChildren = (children) => {
+      return React.Children.map(children, (child:any, i) => {
+        return React.cloneElement(child, {
+          isActive: activeTab === i && isTransitionDone,
+          onFocusChange: handleChangeIndex,
+          focusClass: "defaultFocusCls"
+        });
+      });
+    };
+
+    return (
+      <div className={prefixCls}>
+        <div className={prefixCls+"-content"}>
+            <ScrollingListView 
+                isActive={activeTab === 0} 
+                className="col" 
+                initialSelectedIndex={selectedCol1Index}
+                onChangeIndex={(index) => onCol1ChangeIndex(index)}
+            >
+              {renderChildren(col1Children)}
+            </ScrollingListView>
+            <ScrollingListView 
+                isActive={activeTab === 1} 
+                className="col" 
+                initialSelectedIndex={selectedCol2Index}
+                onChangeIndex={(index) => onCol2ChangeIndex(index)}
+            >
+              {renderChildren(col2Children)}
+            </ScrollingListView>
+            <ScrollingListView 
+              isActive={activeTab === 2} 
+              className="col" 
+              initialSelectedIndex={selectedCol3Index}
+              onChangeIndex={(index) => onCol3ChangeIndex(index)}
+            >
+              {renderChildren(col3Children)}
+            </ScrollingListView>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default TriColListView;
